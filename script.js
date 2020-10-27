b0576baeb00e77992864660c63ed6b8c5d33366f
Rogelio M Corsino <rcorsino@gmail.com>
2020-10-26 22:47:55 -0300

    javascript functionallity

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let popup = document.querySelectorAll(".popup");
-console.log(popup);
+let close = document.querySelector(".output__close");
+let show = document.querySelector(".output.output--hide");
+
 document.addEventListener("DOMContentLoaded", function () {
-  popup[1].innerText = "BUTTON";
-  popup[1].addEventListener("click", function () {
-    alert("You press a button");
+  let popupLength = popup.length;
+  for (let i = 0; i < popupLength; i++) {
+    popup[i].addEventListener("click", function () {
+      let outputText = this.getAttribute("data-message");
+      let outputMessage = document.querySelector(".output__message");
+      outputMessage.innerText = outputText;
+      show.classList.remove("output--hide");
+    });
+  }
+  close.addEventListener("click", function () {
+    show.classList.add("output--hide");
   });
 });
